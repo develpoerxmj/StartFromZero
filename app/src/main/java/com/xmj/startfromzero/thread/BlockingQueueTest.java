be08587d269906e69864fc253a1fc0533e8cdfd7
jiepangzi <2810134546@qq.com>
2020-05-06 15:20:06 +0800

    http

---
+++ app/src/main/java/com/xmj/startfromzero/thread/BlockingQueueTest.java
@@ -0,0 +1,95 @@
+package com.xmj.startfromzero.thread;
+
+import java.util.PriorityQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+
+
+/**
+ * 阻塞队列
+ *
+ * 队列满时，生产者线程阻塞
+ * 队列空时，消费者线程阻塞
+ */
+public class BlockingQueueTest {
+
+    /**
+     * 生产者——消费者
+     */
+
+    private int queueSize = 10;
+    private PriorityQueue<Integer> queue = new PriorityQueue<>(queueSize);
+
+    //阻塞队列
+    ArrayBlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(queueSize);
+
+    public static void main(String[] args) {
+        BlockingQueueTest test = new BlockingQueueTest();
+        Thread pool = new Thread(test.new ThreadPool());
+        Thread put = new Thread(test.new ThreadPut());
+        pool.start();
+        put.start();
+
+
+        Executors.newSingleThreadExecutor();
+    }
+
+    //消费者线程(拿走)
+    public class ThreadPool implements Runnable{
+
+        @Override
+        public void run() {
+            while (true){
+//                synchronized (queue){
+//                    while (queue.size() == 0){
+//                        System.out.println("队列为空");
+//                        try {
+//                            queue.wait();
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                            queue.notify();
+//                        }
+//                    }
+////                  queue.peek();//得到队首元素（不会删除）
+//
+//                    //每次移走队首元素
+//                    System.out.println(queue.poll());
+//                    queue.notify();
+//                }
+
+                try {
+                    System.out.println(blockingQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //生产者线程
+    public class ThreadPut implements Runnable{
+
+        @Override
+        public void run() {
+            while (true){
+//                synchronized (queue){
+//                    while (queue.size() == queueSize){
+//                        try {
+//                            queue.wait();
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                            queue.notify();
+//                        }
+//                    }
+//                    queue.add((int) (Math.random() * 10));
+//                    queue.notify();
+//                }
+                try {
+                    blockingQueue.put((int) (Math.random() * 10));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/xmj/startfromzero/thread/Synchronization.java
+++ app/src/main/java/com/xmj/startfromzero/thread/Synchronization.java
@@ -1,8 +1,12 @@
 package com.xmj.startfromzero.thread;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 同步锁
+ */
 public class Synchronization {
 
     public static void main(String[] args) {
@@ -17,18 +21,37 @@ public class Synchronization {
         public MyRunnable(String name) {
             this.name = name;
         }
+
+        private Lock lock = new ReentrantLock();
+
+        private Condition condition = lock.newCondition();
+
         @Override
         public void run() {
             /**
-             * Synchronized进过编译，会在同步块的前后分别形成monitorenter和monitorexit这个两个字节码指令
+             * Synchronized经过编译，会在同步块的前后分别形成monitorenter和monitorexit这个两个字节码指令
              * 在执行monitorenter指令时，首先要尝试获取对象锁。如果这个对象没被锁定，或者当前线程已经拥有了那个对象锁，把锁的计算器加1
              * 相应的，在执行monitorexit指令时会将锁计算器就减1，当计算器为0时，锁就被释放了
              * 如果获取对象锁失败，那当前线程就要阻塞，直到对象锁被另一个线程释放为止
              */
-            synchronized (this){
-                for (int i = 0; i <10 ; i++) {
+
+//            synchronized (this){
+//                for (int i = 0; i <10 ; i++) {
+//                    System.out.println(name + "==" + i);
+//                }
+//            }
+
+            /**
+             * ReentrantLock
+             * 重入锁：支持重新进入的锁，表示该锁能够支持一个线程对资源的重复加锁
+             */
+            lock.lock();
+            try {
+                for (int i = 0; i < 10; i++) {
                     System.out.println(name + "==" + i);
                 }
+            } finally {
+                lock.unlock();
             }
         }
     }
